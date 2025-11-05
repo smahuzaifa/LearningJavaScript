591bed53657ff4fd9f73581f469c759642dbfda9
smahuzaifa <huzaifasadath@gmail.com>
2025-11-05 11:35:49 +1100

    Strings and its methods

---
+++ basics5.js
@@ -0,0 +1,30 @@
+//Strings in JS
+ let day = "tuesday "
+ console.log(day.slice(0,4))
+ console.log(day[2])
+ let splitDay=day.split("s") //Splits at every s
+ console.log(splitDay)
+ console.log(splitDay[0])
+ console.log(splitDay[1])
+ 
+ //Convert Strings to a number
+ let a='10', b='2';
+ let diff = parseInt(a)-parseInt(b);
+ console.log(diff);
+ //To convert back to string
+ diff.toString();
+ //Concatenate 2 strings
+ let newDay =day+day+day //+ is used to join 2 strings
+ console.log(newDay)
+ let val = newDay.indexOf("day")
+ console.log(newDay.indexOf("day"))
+ console.log(newDay.indexOf("day",5))
+ let count=0;
+ //To find the number of occurrence
+ while(val!==-1){ //While only stops when the condition is false
+    count++
+    val = newDay.indexOf("day",val+1) //Repeat the same from the next index. If val is 4, then it 
+    // starts from 5th index
+ }
+ console.log(count)
+ 
